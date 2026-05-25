83958e988a741abb98d63ca092f962845508a641
FatiMoon81 <fatimalunad@gmail.com>
2026-05-25 17:22:19 -0600

    Changed modal method for another more optimal. Still need to add CCS to it

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -19,3 +19,30 @@ function toggleText() {
     points.style.display = isHidden ? "none" : "inline";
     button.innerHTML = isHidden ? "Show Less" : "Show More";
  }
+
+function openModal(imgNum) {
+  document.body.classList.add("stop-scrolling");
+  switch(imgNum) {
+    case 1:
+      document.getElementById("imgContainerModal").src = "../img/Jujutsu_High.png";
+      document.getElementById("caption").innerHTML = "Tokyo High";
+      break;
+    case 2:
+      document.getElementById("imgContainerModal").src = "../img/TiendaDiscos.png";
+      document.getElementById("caption").innerHTML = "Tienda de Discos";
+      break;
+    case 3:
+      document.getElementById("imgContainerModal").src = "../img/UbicaTec.png";
+      document.getElementById("caption").innerHTML = "UbicaTec";
+      break;
+    default:
+      // code block
+  }
+  document.getElementById("imgModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("imgModal").style.display = "none";
+  document.body.classList.remove("stop-scrolling");
+}
